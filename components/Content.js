74bbacd87fba43de581df5736c583580bcca9290
AmorRui <teng0009@126.com>
2018-05-11 22:48:42 +0800

    “数据持久化”

--- components/Content.js
+++ components/Content.js
@@ -11,18 +11,29 @@ export default class Comtent extends React.Component {
       list:[]
     }
   }
+  // 持久化保存数据。 页面刷新数据仍然存在
+  componentWillMount () {
+
+    //  this.state.list  = JSON.parse(localStorage.getItem('task'));
+    //  如果list 是空的。 map 会为空。 需要进行判断
+    this.state.list  = JSON.parse(localStorage.getItem('tasks')||('[]'));
+  }
   // 添加事件
   addTask = (task) => {
     // 传递数据， 执行render方法进行调用
     this.state.list.push(task);
     this.setState({
     })
+    // 1》数据持久化 localStorage
+    //  Application（local Stronge） 会进行显示数据
+    localStorage.setItem('tasks',JSON.stringify(this.state.list))
   }
   render () {
     return <div>
       <section className="todoapp">
         <Add addTask =  {this.addTask }/>
-        <List />
+        {/* 2》回传数据 */}
+        <List tasklist = { this.state.list }/>
         <Footer />
       </section>
     </div>
--- components/List.js
+++ components/List.js
@@ -6,23 +6,25 @@ export default class List extends React.Component {
       <section className="main">
 				<input id="toggle-all" className="toggle-all" type="checkbox" />
 				<label htmlFor="toggle-all">Mark all as complete</label>
-				<ul className="todo-list">		
-					<li className="completed">
-						<div className="view">
-							<input className="toggle" type="checkbox" checked />
-							<label>Taste JavaScript</label>
-							<button className="destroy"></button>
-						</div>
-						<input className="edit" value="Create a TodoMVC template" />
-					</li>
-					<li>
-						<div className="view">
-							<input className="toggle" type="checkbox" />
-							<label>Buy a unicorn</label>
-							<button className="destroy"></button>
-						</div>
-						<input className="edit" value="Rule the web" />
-					</li>
+				<ul className="todo-list">
+					{/* 3》》动态生成li 使用map方法*/}
+					{
+						// 5》》 className={ item.completed?"completed":"" }
+						// 6》》 key={ item.id }
+						this.props.tasklist.map((item,i) => {
+							return <li className={ item.completed?"completed":"" } key={ item.id }>
+									<div className="view">
+									{/* 7 》》 checked = { item.completed }
+									8 》》 刷新页面，之前的数据已经消失。 需要再次保存数据*/}
+										<input className="toggle" type="checkbox" checked = { item.completed }/>
+										{/* 4》》 item.name  显示 */}
+										<label>{ item.name }</label>
+										<button className="destroy"></button>
+									</div>
+									<input className="edit" value="Create a TodoMVC template" />
+								</li> 
+						})
+					}
 				</ul>
 			</section>
     </div>
