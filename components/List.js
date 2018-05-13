b4d0da9754399a85541a527f964c5dada78305fa
AmorRui <teng0009@126.com>
2018-05-13 15:45:06 +0800

    编辑数据。 双击获取焦点。失去焦点

--- components/List.js
+++ components/List.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default class List extends React.Component {
+	constructor () {
+		super()
+		this.state = {
+			eid : -1
+		}
+	}
   render () {
     return <div>
       <section className="main">
@@ -12,7 +18,17 @@ export default class List extends React.Component {
 						// 5》》 className={ item.completed?"completed":"" }
 						// 6》》 key={ item.id }
 						this.props.tasklist.map( (item,i) => {
-							return <li className={ item.isCompleted?"completed":"" } key={ item.id }>
+							return <li className={ (item.isCompleted?"completed":"")+(item.id === this.state.eid ? 'editing':'') } onDoubleClick = {
+								() => {
+									// 让内容可以进行编辑
+									this.setState({
+										eid:item.id
+									}),() => {
+										//  双击出现光标
+											this.refs[item.id].focus();
+									}
+								}
+							} key={ item.id }>
 									<div className="view">
 									{/* 7 》》 checked = { item.completed }
 									8 》》 刷新页面，之前的数据已经消失。 需要再次保存数据*/}
@@ -28,7 +44,17 @@ export default class List extends React.Component {
 											this.props.delTask( item.id )
 										} }></button>
 									</div>
-									<input className="edit" value="Create a TodoMVC template" />
+									<input ref ={ item.id } className="edit" onChange= { 
+										// 编辑数据更新
+										(e)=> {
+										item.name = e.target.value;
+										this.props.upData();
+									} } value={item.name} onBlur = {()=> {
+										// 失去焦点
+										this.setState({
+											eid = -1
+										})
+									} }/>
 								</li> 
 						})
 					}
--- readme.js
+++ readme.js
@@ -11,3 +11,9 @@ Content -- 内部功能
 4. 将数据存储在Content 中  数据存储在localStronge中
       要在Content中初始化State对象
 5. 实现Add的功能
+   a 将添加数据的文本框绑定到state对象中
+   b 敲回车提交数据   form表单的onSubmit---> 阻止默认事件   向父组件传递数据 （3步）  不要忘了将数据放到localStorage中
+6. 在组件第一次创建的时候  要从localStorage中 读取数据
+7. 以属性的方式 将Content中state里面的list数据 传递给List组件， 在List组件中 展示数据
+8. 将CHheckbox进行双向数据绑定  更新isCompleted就是更新了父组件中的数据  只需要让父组件更新就可以了
+9. 删除功能    子组件向父组件传递要删除数据的id   在父组件中通过filter方法对数组数据进行过滤
